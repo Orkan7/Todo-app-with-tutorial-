83267cfead194926aab2d258a35b42345995a48a
musak <musakantar2009@gmail.com>
2024-01-03 00:07:31 +0100

    Basis GUI (ohne Interaktionen) Alhamdulillah

--- GUI.py
+++ GUI.py
@@ -2,4 +2,14 @@
 
 
 
-import Modulescripts.functionsforMain
+import Modulescripts.functionsforMain as Functions
+import PySimpleGUI as GUI
+
+label = GUI.Text("Type in a to-do")
+input_box = GUI.InputText(tooltip="Enter todo")
+Button = GUI.Button("Add")
+
+Window = GUI.Window("My To-Do App", layout=[[label], [input_box, Button]])
+Window.read()
+print("Hello")
+Window.close()

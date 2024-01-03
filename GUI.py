77fe83a91b39c70971eeb9f39087b0990fe22f06
musak <musakantar2009@gmail.com>
2024-01-03 21:40:01 +0100

    Bismillah
    Saving

--- GUI.py
+++ GUI.py
@@ -9,7 +9,6 @@ label = GUI.Text("Type in a to-do")
 input_box = GUI.InputText(tooltip="Enter todo")
 Button = GUI.Button("Add")
 
-Window = GUI.Window("My To-Do App", layout=[[label], [input_box, Button]])
+Window = GUI.Window("My To-Do App", layout=[[[label], input_box, Button]])
 Window.read()
-print("Hello")
 Window.close()
